8301b3327456efa05d884f698aeede524b878ea2
zhangjrgary <zhangjrgary@gmail.com>
2016-03-23 02:01:51 +0800

    Add

---
+++ README.md
@@ -0,0 +1,17 @@
+Tahm --- a O2O catering application with MEAN architecture, and codes are deposited on Bitbucket with services  deployed on Bluemix.
+This application allows you to display, order and pay, with the results updating in real-time. 
+
+The app uses the following libraries and frameworks:
+Node.js
+Express
+Jade templates
+Mongoose
+AngularJS
+Bootstrap 3
+Socket.io
+
+Demo
+You can see the app running live at http://******.bluemix.net/.
+
+License
+The source code for the app is available under the MIT license, which is found in license.txt in the root of this repository.
---
+++ app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// app 本身有很多方法，其中包括最常用的 get、post、put/patch、delete，在这里我们调用其中的 get 方法，为我们的 `/` 路径指定一个 handler 函数。
+// 这个 handler 函数会接收 req 和 res 两个对象，他们分别是请求的 request 和 response。
+// request 中包含了浏览器传来的各种信息，比如 query 啊，body 啊，headers 啊之类的，都可以通过 req 对象访问到。
+// res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息，比如 header 信息，比如想要向浏览器输出的内容。这里我们调用了它的 #send 方法，向浏览器输出一个字符串。
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+// 定义好我们 app 的行为之后，让它监听本地的 3000 端口。这里的第二个函数是个回调函数，会在 listen 动作成功后执行，我们这里执行了一个命令行输出操作，告诉我们监听动作已完成。
+app.listen(3000, function () {
+  console.log('app is listening at port 3000');
+});
+
+module.exports = app;
